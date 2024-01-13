97590dd54d164c391b0ea108374b8f75866a33cf
Alexander Yurin <hiimmandarin32@gmail.com>
2024-01-13 15:54:43 +0300

    Добавлена ссылки к каждой категории

--- goods/urls.py
+++ goods/urls.py
@@ -2,13 +2,13 @@ from django.conf.urls.static import static
 from django.urls import path
 
 from app.settings import MEDIA_URL, MEDIA_ROOT
-from goods.views import CatalogList, ProductDetail
+from goods.views import CategoryDetail, ProductDetail
 
 
 app_name = "catalog"
 
 
 urlpatterns = [
-    path("", CatalogList.as_view(), name="catalog"),
+    path("<slug:cat_slug>/", CategoryDetail.as_view(), name="catalog"),
     path("product/<slug:slug>/", ProductDetail.as_view(), name="product")
 ]
--- goods/views.py
+++ goods/views.py
@@ -1,18 +1,31 @@
+from django.core.paginator import Paginator
 from django.http import HttpRequest, HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_list_or_404
 from django.views.generic import ListView, DetailView
 
 from goods.models import Categories, Products
 
 
-class CatalogList(ListView):
+class CategoryDetail(DetailView):
 	model = Products
-	context_object_name = "products"
 	template_name = "goods/catalog.html"
+	slug_url_kwarg = "cat_slug"
+	context_object_name = "products"
 
+	def get_object(self, queryset=None):
+		slug = self.kwargs.get(self.slug_url_kwarg)
+
+
+
+
+		if slug == "all":
+			queryset = Products.objects.all()
+		else:
+			queryset = get_list_or_404(Products.objects.filter(id_category__slug=slug))
+
+		return queryset
 
 class ProductDetail(DetailView):
 	model = Products
 	template_name = "goods/product.html"
 	context_object_name = "product"
-
